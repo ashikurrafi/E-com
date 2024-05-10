ba182016ced2cf311de50cab6b360acfe55371ab
Ashikur Rafi <ashikurrafi@gmail.com>
2024-05-10 18:02:28 +0600

    Login and Register page added
    Added login and register page, both are connected through "NavLink" and created file structure.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import Router from "./router/Router";
+import publicRoutes from "./router/routes/publicRoutes";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+  console.log(publicRoutes);
+  return <Router allRoutes={allRoutes} />;
 }
 
 export default App;
