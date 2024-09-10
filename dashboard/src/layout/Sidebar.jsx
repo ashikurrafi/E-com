ac51fbb9d9fa5d88eed0314d0ce0c79e9b38b372
Ashikur Rafi <ashikurrafi@gmail.com>
2024-09-11 01:19:46 +0600

    Add Header.jsx
    Not working

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,102 +1,69 @@
-// import { useEffect, useState } from "react";
-// import { Link, useLocation } from "react-router-dom";
-// import getNavs from "./../navigation/index";
-
-// const Sidebar = () => {
-//   const { pathname } = useLocation();
-
-//   const [allNav, setAllNav] = useState([]);
-
-//   useEffect(() => {
-//     const navs = getNavs("admin");
-//     setAllNav(navs);
-//   }, []);
-
-//   console.log(allNav);
-//   console.log(pathname);
-
-//   return (
-//     <div>
-//       <div></div>
-//       <div className="w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/5%)] transition-all">
-//         <div className="h-[70px] flex justify-center items-center">
-//           <Link to="/" className="w-[180px] h-[50px]">
-//             <img
-//               className="w-full h-full"
-//               src="http://localhost:3000/Images/logo.png"
-//               alt="Logo"
-//             />
-//           </Link>
-//         </div>
-//         <div className="px-[16px]">
-//           <ul>
-//             {allNav.map((n, i) => (
-//               <li key={i}>
-//                 <Link
-//                   to={n.path}
-//                   className={`${
-//                     pathname === n.path
-//                       ? "bg-slate-600 shadow-indigo-500/30 text-white duration-500 "
-//                       : "text-[#d0d2d6] font-normal duration-200"
-//                   } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 `}
-//                 >
-//                   <span>{n.icon}</span>
-//                   <span>{n.title}</span>
-//                 </Link>
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import allNav from "../navigation/allNav";
+import { BiLogInCircle } from "react-icons/bi";
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar, setShowSidebar }) => {
   const { pathname } = useLocation();
   const [allNavItems, setAllNavItems] = useState([]);
 
   useEffect(() => {
-    const role = "admin"; // Replace this with dynamic role if needed
+    const role = "admin";
     const filteredNavs = allNav.filter((nav) => nav.role === role);
     setAllNavItems(filteredNavs);
   }, []);
 
   return (
-    <div className="w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/5%)] transition-all">
-      <div className="h-[70px] flex justify-center items-center">
-        <Link to="/" className="w-[180px] h-[50px]">
-          <img
-            className="w-full h-full"
-            src="http://localhost:3000/Images/logo.png"
-            alt="Logo"
-          />
-        </Link>
-      </div>
-      <div className="px-[16px]">
-        <ul>
-          {allNavItems.map((n) => (
-            <li key={n.id}>
-              <Link
-                to={n.path}
-                className={`${
-                  pathname === n.path
-                    ? "bg-slate-600 shadow-indigo-500/30 text-white duration-500"
-                    : "text-[#d0d2d6] font-normal duration-200"
-                } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1`}
-              >
-                <span>{n.icon}</span>
-                <span>{n.title}</span>
-              </Link>
+    <div>
+      <div
+        onClick={() => setShowSidebar(false)}
+        className={`fixed duration-200 ${
+          !showSidebar ? "invisible" : "visible"
+        } w-screen h-screen bg-[#22292f80] top-0 left-0 z-10`}
+      ></div>
+
+      <div
+        className={`w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
+          showSidebar ? "left-0" : "-left-[260px] lg:left-0"
+        }`}
+      >
+        <div className="h-[70px] flex justify-center items-center">
+          <Link to="/" className="w-[180px] h-[50px]">
+            <img
+              className="w-full h-full"
+              src="http://localhost:3000/Images/logo.png"
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        <div className="px-[16px]">
+          <ul>
+            {allNavItems.map((n) => (
+              <li key={n.id}>
+                <Link
+                  to={n.path}
+                  className={`${
+                    pathname === n.path
+                      ? "bg-slate-600 shadow-indigo-500/30 text-white duration-500"
+                      : "text-[#d0d2d6] font-normal duration-200"
+                  } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1`}
+                >
+                  <span>{n.icon}</span>
+                  <span>{n.title}</span>
+                </Link>
+              </li>
+            ))}
+
+            <li>
+              <button className="text-[#d0d2d6] font-normal duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 ">
+                <span>
+                  <BiLogInCircle />
+                </span>
+                <span>Logout</span>
+              </button>
             </li>
-          ))}
-        </ul>
+          </ul>
+        </div>
       </div>
     </div>
   );
