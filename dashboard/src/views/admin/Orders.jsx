11293f1b0c5f7b53c60bf603022c71e7417ae175
Ashikur Rafi <ashikurrafi@gmail.com>
2024-09-25 01:09:57 +0600

    Add admin category page
    Not functional

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -16,7 +16,10 @@ const Orders = () => {
     <div className="px-2 lg:px-7 pt-5">
       <div className="w-full p-4  bg-[#283046] rounded-md">
         <div className="flex justify-between items-center">
-          <select className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]">
+          <select
+            onChange={(e) => setParPage(parseInt(e.target.value))}
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]"
+          >
             <option value="5">5</option>
             <option value="5">15</option>
             <option value="5">25</option>
